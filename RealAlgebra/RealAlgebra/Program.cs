ffa25734f80346f2bf774991171c2b42335568c1
deepakk2000 <deepakjc112000@gmal.com>
2023-11-23 16:21:39 +0530

    A-19

---
+++ RealAlgebra/RealAlgebra/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeLib;
+
+namespace RealAlgebra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter\n1.Square,\n2.Reactangle,\n3.Circle,\n4.Triangle");
+            int value = int.Parse(Console.ReadLine());
+            switch (value)
+            {
+                case 1:
+                    Console.WriteLine("Enter side value ");
+                    int side = int.Parse(Console.ReadLine());
+                    Console.WriteLine(Square.Area(side));
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Length value ");
+                   
+                    int Length = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Width value ");
+                    int Width = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine(Reactangle.Area(Length,Width));
+                    break;
+                case 3:
+                    Console.WriteLine("Enter Radious value ");
+                    int Radious = int.Parse(Console.ReadLine());
+                    Console.WriteLine(Circle.Area(Radious));
+                    break;
+                case 4:
+                    Console.WriteLine("Enter Base value ");
+                    int Base = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Height value ");
+                    int Height = int.Parse(Console.ReadLine());
+                    Console.WriteLine(Triangle.Area(Base,Height));
+                    break;
+
+            }
+            
+            
+            
+            
+           
+            
+            
+            
+        }
+    }
+}
---
+++ RealAlgebra/ShapeLib/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLib
+{
+    public static class Reactangle
+    {
+
+        public static double Area(int Length, int Width)
+        {
+            return Length * Width;
+        }
+
+    }
+
+    public static class Square
+    {
+
+        public static double Area(int Side)
+        {
+            return Side * Side;
+        }
+    }
+    public static class Circle
+    {
+
+        public static double Area(int Radius)
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+    public static class Triangle
+    {
+
+        public static double Area(int Base, int Height)
+        {
+            return 0.5 * Base * Height;
+        }
+    }
+    
+}
